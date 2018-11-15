43d3ae98bab9aa652dff78730e017073b9a4d59f
marc.palm <info@marc.palm.de>
2018-11-15 12:14:09 +0100

    #preloader

--- js/fresh.js
+++ js/fresh.js
@@ -2,11 +2,11 @@ $(document).ready(function () {
   var $window = $(window)
 
   //Preloader
-  $window.on('load', function () { // makes sure the whole site is loaded
-    $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(350).css({ 'overflow': 'visible' });
-  })
+  // $window.on('load', function () { // makes sure the whole site is loaded
+  //   $('#status').fadeOut(); // will first fade out the loading animation
+  //   $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+  //   $('body').delay(350).css({ 'overflow': 'visible' });
+  // })
 
   var $navbarBurger = $('.navbar-burger')
   var $navbarClone = $('#navbar-clone')
