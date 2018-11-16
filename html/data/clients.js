9e55f7eb4663082a783adaceb5d62fe842360e89
marc.palm <info@marc.palm.de>
2018-11-16 09:22:29 +0100

    clients moved to data

---
+++ html/data/clients.js
@@ -0,0 +1,20 @@
+module.exports = [{
+  title: 'Our client love us',
+  subtitle: 'Because we got the extra mile for them',
+  list: [{
+    name: 'Irma Bradley',
+    role: 'Product Owner',
+    image: '1',
+    text: 'I care about the user experience first and foremost. Is the feature still working? SEO configured correctly? Now I can actually write the integration tests with no extra effort.'
+  }, {
+    name: 'John Bradley',
+    role: 'CTO',
+    image: '2',
+    text: 'Our team loves the tight integration with Git. Engineers can focus on writing and delivering code while testless ensures the quality. Tests can be recorded by everyone. No training required.'
+  }, {
+    name: 'Gary Blackman',
+    role: 'QA Manager',
+    image: '3',
+    text: 'In the past, we had to write &amp; maintain test manually. Tests were flakky. testless is much more reliable and abstract a lot of noise away. We can focus now on what to test, not on the how.'
+  }]
+}]
--- html/data/features.js
+++ html/data/features.js
@@ -1,39 +1,43 @@
 module.exports = [{
-  title: 'Record an replay',
-  image: 'laptop-globe',
-  bullets: [
-    'Record tests by browsing',
-    'Designed for non-techies',
-    'Human readable tests',
-    'Detect visual differences',
-    'Be alerted in case of changes',
-    'Simply confirm or flag warnings'
-  ]
-}, {
-  title: 'Sync with versioning',
-  image: 'doc-sync',
-  bullets: [
-    'Share tests real time',
-    'Switch versions',
-    'Recover old versions'
-  ]
-}, {
-  title: 'Cloud monitoring',
-  image: 'plug-cloud',
-  bullets: [
-    'Run tests in the cloud',
-    'All browsers',
-    'Regularly or upon demand',
-    'Monitoring & alerting',
-    'Dashboards'
-  ]
-}, {
-  title: 'Integration',
-  image: 'mobile-feed',
-  bullets: [
-    'Supports CI/CD tooling via Docker',
-    'Outsource automated testing',
-    'Tests are synced with Git',
-    'Extendable customizable DSL'
-  ]
+  title: 'You care about quality',
+  subtitle: 'Let us help out here',
+  list: [{
+    title: 'Record & replay',
+    image: 'laptop-globe',
+    bullets: [
+      'Record tests by browsing',
+      'Designed for non-techies',
+      'Human readable tests',
+      'Detect visual differences',
+      'Be alerted in case of changes',
+      'Simply confirm or flag warnings'
+    ]
+  }, {
+    title: 'Sync- & versioning',
+    image: 'doc-sync',
+    bullets: [
+      'Share tests real time',
+      'Switch versions',
+      'Recover old versions'
+    ]
+  }, {
+    title: 'Cloud monitoring',
+    image: 'plug-cloud',
+    bullets: [
+      'Run tests in the cloud',
+      'All browsers',
+      'Regularly or upon demand',
+      'Monitoring & alerting',
+      'Dashboards'
+    ]
+  }, {
+    title: 'Integration',
+    image: 'mobile-feed',
+    bullets: [
+      'Supports CI/CD tooling via Docker',
+      'Outsource automated testing',
+      'Tests are synced with Git',
+      'Extendable customizable DSL'
+    ]
+  }]
 }]
