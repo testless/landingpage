667b02a0039e9f1d4fa7a4bb423ba54c463158a7
marc.palm <info@marc.palm.de>
2018-11-16 09:02:01 +0100

    make features responsive

---
+++ html/data/features.js
@@ -0,0 +1,39 @@
+module.exports = [{
+  title: 'Record an replay',
+  image: 'laptop-globe',
+  bullets: [
+    'Record tests by browsing',
+    'Designed for non-techies',
+    'Human readable tests',
+    'Detect visual differences',
+    'Be alerted in case of changes',
+    'Simply confirm or flag warnings'
+  ]
+}, {
+  title: 'Sync with versioning',
+  image: 'doc-sync',
+  bullets: [
+    'Share tests real time',
+    'Switch versions',
+    'Recover old versions'
+  ]
+}, {
+  title: 'Cloud monitoring',
+  image: 'plug-cloud',
+  bullets: [
+    'Run tests in the cloud',
+    'All browsers',
+    'Regularly or upon demand',
+    'Monitoring & alerting',
+    'Dashboards'
+  ]
+}, {
+  title: 'Integration',
+  image: 'mobile-feed',
+  bullets: [
+    'Supports CI/CD tooling via Docker',
+    'Outsource automated testing',
+    'Tests are synced with Git',
+    'Extendable customizable DSL'
+  ]
+}]
