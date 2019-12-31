c2e4e006bba23e89623f501b745f9fe3c709ebeb
marc.palm <info@marc.palm.de>
2019-12-31 19:42:57 +0100

    feat: apply button

--- html/data/navbar.js
+++ html/data/navbar.js
@@ -1,16 +1,9 @@
-const countrycodes = require('./countrycodes')
 
 module.exports = [
   {
     id: 'navbar',
     base: 'no-shadow',
     menu: 'is-static',
-    countrycodes
-  },
-  {
-    id: 'navbar-clone',
-    base: '',
-    menu: 'is-fixed',
     countrycodes
   }
 ]
