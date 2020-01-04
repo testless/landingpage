d220a65cb14eb90899bfff60bc6ea93f3ff62b1c
marc.palm <info@marc.palm.de>
2020-01-04 17:43:56 +0100

    docs: update docs

--- html/data/features.js
+++ html/data/features.js
@@ -1,73 +1,116 @@
-module.exports = [{
-  title: 'Empowering tech talent to deliver value',
-  subtitle: 'That\'s what gets our blood pumping!',
-  list: [{
-    title: 'Transparency',
-    image: 'undraw/finish_line',
-    bullets: [
-      'Transparency enables trust',
-      'Trust enables speed',
-      'Speed enables fast iteration and value'
-    ]
-  }, {
-    title: 'Simplicity',
-    image: 'undraw/fast_loading',
-    bullets: [
-      'Simple solutions are not easy, but faster to build',
-      'Simple solutions require a solid understanding',
-      'Simple solutions are easier to maintain, scale and teach',
-    ]
-  }, {
-    title: 'Beginners mind',
-    image: 'undraw/runner_start',
-    bullets: [
-      'Focus on 10x improvements instead of increments first',
-      'What Would This Look Like If It Were Simple?',
-      'Trust your gut feeling, but verify',
-    ]
-  },  {
-    title: 'Focus',
-    image: 'undraw/target',
-    bullets: [
-      'Have few targets',
-      'Be crystal clear on your focus',
-      'Execute relentlessly',
-    ]
-  }]
-}, {
-  title: 'What we offer',
-  subtitle: 'We provide state of the art tech solutions',
-  list: [{
-    title: 'Tech consulting & solution design',
-    image: 'undraw/business_deal',
-    bullets: [
-      'Business mindset',
-      'Focus on value created for customers',
-      'Focus on time to market'
-    ]
-  }, {
-    title: 'Technical project management',
-    image: 'undraw/creation_process',
-    bullets: [
-      'Agile project management with little overhead',
-      'Focus on value created per unit of time',
-      'No-bullshit approach'
-    ]
-  }, {
-    title: 'Cloud & web development',
-    image: 'undraw/cloud_hosting',
-    bullets: [
-      'Top of notch web experiences',
-      'Cloud based, designed for scale',
-      'High end tech stack'
-    ]
-  }, {
-    title: 'Improving speed',
-    image: 'undraw/good_team',
-    bullets: [
-      'Consulting small to medium sized teams',
-      'Tradeoffs between perfection, quality and speed',
-      'Infiltrating ownership and business mindset',
-    ]
-  }]
-}]
+module.exports = [
+  {
+    title: 'Empowering talent to deliver',
+    subtitle: 'Our values define how we work',
+    list: [
+      {
+        title: 'Transparency enables trust',
+        image: 'undraw/finish_line',
+        quotes: [
+          {
+            q:
+              'How would they feel about any given action if they know it was to be written up the next day in their local newspaper.',
+            by: 'Warren Buffett',
+          },
+          {
+            q:
+              'By the standards of the rest of the world, we overtrust. So far it has worked very well for us.',
+            by: 'Charlie Munger',
+          },
+          {
+            q:
+              'Earn trust, earn trust, earn trust. Then you can worry about the rest.',
+            by: 'Seth Godin',
+          },
+        ],
+      },
+      {
+        title: 'Simplicity is the ultimate sophistication',
+        image: 'undraw/fast_loading',
+        quotes: [
+          {
+            q:
+              "That's been one of my mantras - focus and simplicity. Simple can be harder than complex: You have to work hard to get your thinking clean to make it simple.",
+            by: 'Steve Jobs',
+          },
+          { q: 'What would this look like if it were easy?', by: 'Tim Ferris' },
+          { q: 'If It’s Not Simple, It’s Bullshit.', by: 'dailystoic.com' },
+        ],
+      },
+      {
+        title: "Start with a beginner's mind",
+        image: 'undraw/runner_start',
+        quotes: [
+          {
+            q:
+              "In the beginner's mind there are many possibilities, in the expert's mind there are few",
+            by: 'Shunryu Suzuki',
+          },
+          {
+            q:
+              'Having a beginner’s mind informs me. I’m trying to listen deeply & the beginner’s mind is informing me to step back, so that I can create what wants to be, not what was. The future does not equal the past.',
+            by: 'Marc Benioff',
+          },
+        ],
+      },
+      {
+        title: 'Focus on few things',
+        image: 'undraw/target',
+        quotes: [
+          {
+            q:
+              'It’s not the daily increase but daily decrease. Hack away at the unessential.',
+            by: 'Bruce Lee',
+          },
+          {
+            q:
+              'What factor do you feel has been the most important in getting to where you’ve gotten in life? Buffett immediately replied: Focus. Bill Gates agreed.',
+            by: 'Some dinner in July 1991.',
+          },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'What we offer',
+    subtitle: 'We provide state of the art tech solutions',
+    list: [
+      {
+        title: 'Tech consulting & solution design',
+        image: 'undraw/business_deal',
+        bullets: [
+          'Business mindset',
+          'Focus on value created for customers',
+          'Focus on time to market',
+        ],
+      },
+      {
+        title: 'Technical project management',
+        image: 'undraw/creation_process',
+        bullets: [
+          'Agile project management with little overhead',
+          'Focus on value created per unit of time',
+          'No-bullshit approach',
+        ],
+      },
+      {
+        title: 'Cloud & web development',
+        image: 'undraw/cloud_hosting',
+        bullets: [
+          'Top notch web experiences',
+          'Cloud based, designed for scale',
+          'High end tech stack',
+        ],
+      },
+      {
+        title: 'Improving speed',
+        image: 'undraw/good_team',
+        bullets: [
+          'Consulting small to medium sized teams',
+          'Balancing perfection, quality and speed',
+          'Infiltrating ownership and business mindset',
+        ],
+      },
+    ],
+  },
+];
